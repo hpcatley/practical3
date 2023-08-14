fd9888b0b0bfa9016a92ce59243acff72ddad4ac
hpcatley <hpcatley@gmail.com>
2023-08-14 20:39:28 +0930

    hi

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -6,7 +6,7 @@ int Truckloads::numTrucks(int numCrates, int loadSize) {
         return 1;
     }
     else {
-        return numTrucks(numCrates/2, loadSize) + numTrucks(numCrates/2, loadSize);
+        return numTrucks(numCrates/2, loadSize) + numTrucks(numCrates/2, loadSize) + numTrucks(numCrates%2, loadSize);
     }
 }
 
